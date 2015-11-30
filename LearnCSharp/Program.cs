45df7707757035be6938fa69f54f084a3daef0e2
xu chen <xu chen>
2015-11-30 19:28:11 +0800

    TmodKiller

---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace LearnCSharp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            string path = "";
+            OpenFileDialog selfiles = new OpenFileDialog();
+            selfiles.FileName = "Open Tmodloader File";
+            selfiles.Filter = "tmodloader Files (*.tmod)|*.tmod";
+            DialogResult da = selfiles.ShowDialog();
+            if (da == DialogResult.OK)
+            {
+                path = selfiles.FileName;
+            }
+            else
+            {
+                Console.WriteLine("No file selected!");
+                return;
+            }
+            try
+            {
+                string str;
+                string fileName;
+                //string path = Directory.GetCurrentDirectory() + "\\ExampleMod.tmod"; Console.WriteLine(path);
+                TmodIO tmod = new TmodIO(path);
+                tmod.Read();
+                if (tmod.InvalidFile)
+                {
+                    throw new FileLoadException();
+                }
+                if (!tmod.HasFile("All"))
+                {
+                    byte[] buffer = tmod.GetFile("Windows");
+                    byte[] buffer2 = tmod.GetFile("Other");
+                    byte[] buffer3 = tmod.GetFile("Resources");
+
+
+                    using (MemoryStream mem = new MemoryStream(buffer3))
+                    {
+                        using (BinaryReader br = new BinaryReader(mem))
+                        {
+                            mem.Seek(br.ReadInt32(), SeekOrigin.Current);
+                            str = br.ReadString();
+                            #region DEbug！
+                            fileName = string.Format("\\{0}.dll", str);
+                            File.WriteAllBytes(Directory.GetCurrentDirectory() + fileName, buffer);
+                            Console.WriteLine(string.Format("Extracting \\{0}.dll", str));
+                            fileName = string.Format("\\{0}.dll", str + "2");
+                            File.WriteAllBytes(Directory.GetCurrentDirectory() + fileName, buffer2);
+                            Console.WriteLine(string.Format("Extracting \\{0}.dll", str + "2"));
+                            //fileName =string.Format("{0}",str);
+                            #endregion
+                            DirectoryInfo dir = Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\" + str);
+                            for (string str2 = br.ReadString(); str2 != "end"; str2 = br.ReadString())
+                            {
+                                byte[] buf = br.ReadBytes(br.ReadInt32());
+                                if (!Directory.Exists(str2))
+                                {
+                                    Directory.CreateDirectory(Path.GetDirectoryName(str2));
+
+                                }
+
+                                using (FileStream fl = File.Create(Directory.GetCurrentDirectory() + "\\" + str2))
+                                {
+                                    using (MemoryStream stream2 = new MemoryStream(buf))
+                                    {
+                                        Console.WriteLine(string.Format("Extracting \\{0}", Directory.GetCurrentDirectory() + "\\" + str2));
+                                        fl.Write(stream2.ToArray(), 0, stream2.ToArray().Length);
+                                    }
+
+                                }
+
+                            }
+                            //mem.Dispose();
+                        }
+
+                    }
+
+
+                }
+                else
+                {
+                    throw new FileLoadException();
+                }
+                Console.WriteLine("Done!");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+
+        }
+
+    }
+}
---
+++ LearnCSharp/TmodIO.cs
@@ -0,0 +1,114 @@
+﻿using Ionic.Zlib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LearnCSharp
+{
+    public class TmodIO
+    {
+        public  IList<string> fileNames = new List<string>();
+
+        public  IDictionary<string, byte[]> files = new Dictionary<string, byte[]>();
+
+        public readonly string Name;
+
+        public bool InvalidFile
+        {
+            get;
+            internal set;
+        }
+
+        internal TmodIO(string name)
+        {
+            this.Name = name;
+            this.InvalidFile = false;
+        }
+
+        internal bool HasFile(string fileName)
+        {
+            return this.files.ContainsKey(fileName);
+        }
+
+        internal void AddFile(string fileName, byte[] data)
+        {
+            if (!this.HasFile(fileName))
+            {
+                this.fileNames.Add(fileName);
+            }
+            byte[] array = new byte[data.Length];
+            data.CopyTo(array, 0);
+            this.files[fileName] = array;
+        }
+
+        internal void RemoveFile(string fileName)
+        {
+            if (this.HasFile(fileName))
+            {
+                this.fileNames.Remove(fileName);
+                this.files.Remove(fileName);
+            }
+        }
+
+        internal byte[] GetFile(string fileName)
+        {
+            if (this.HasFile(fileName))
+            {
+                return this.files[fileName];
+            }
+            return null;
+        }
+
+        internal void Save()
+        {
+            using (FileStream fileStream = File.Create(this.Name))
+            {
+                using (DeflateStream deflateStream = new DeflateStream(fileStream, CompressionMode.Compress))
+                {
+                    using (BinaryWriter binaryWriter = new BinaryWriter(deflateStream))
+                    {
+                        binaryWriter.Write((byte)this.fileNames.Count);
+                        foreach (string current in this.fileNames)
+                        {
+                            binaryWriter.Write(current);
+                            byte[] array = this.files[current];
+                            binaryWriter.Write(array.Length);
+                            binaryWriter.Write(array);
+                        }
+                    }
+                }
+            }
+        }
+
+        internal void Read()
+        {
+            try
+            {
+                using (FileStream fileStream = File.OpenRead(this.Name))
+                {
+                    using (DeflateStream deflateStream = new DeflateStream(fileStream, CompressionMode.Decompress))
+                    {
+                        using (BinaryReader binaryReader = new BinaryReader(deflateStream))
+                        {
+                            int num = (int)binaryReader.ReadByte();
+                            for (int i = 0; i < num; i++)
+                            {
+                                this.AddFile(binaryReader.ReadString(), binaryReader.ReadBytes(binaryReader.ReadInt32()));
+                            }
+                        }
+                        deflateStream.Dispose();
+                    }
+                }
+            }
+            catch
+            {
+                this.InvalidFile = true;
+            }
+        }
+
+        internal bool ValidMod()
+        {
+            return !this.InvalidFile && this.HasFile("Resources") && ((this.HasFile("Windows") && this.HasFile("Other")) || this.HasFile("All"));
+        }
+    }
+}
